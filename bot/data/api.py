093347b98b72b9156117a1ec81aeae5a5fb20340
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-11-20 19:11:12 +0500

    update api

--- bot/data/api.py
+++ bot/data/api.py
@@ -1,20 +1,31 @@
 import requests
+from data.config import BASE_URL
 import json
 
-BASE_URL = 'https://mamatmusayev.uz/api/'
-
-
 def create_user(telegram_id, username, full_name, is_active):
-    url = BASE_URL + 'users/'
+    url = BASE_URL + 'botuser/'
     data = {
-        'telegram_id': telegram_id,
+        'tg_id': telegram_id,
         'username': username,
         'full_name': full_name,
         'is_active': is_active
     }
     response = requests.post(url, data=data)
+    print(response.json())
+    return response.status_code
+
+def update_phone(telegram_id, phone):
+    url = BASE_URL + f'botuser/{telegram_id}/'
+    data = {
+        'phone': phone
+    }
+    response = requests.patch(url, data=data)
+    return response.status_code
     # print(response.json())
-
-
+    
+a = create_user(telegram_id=223, username='username', full_name='full_name', is_active=True)
+print(a)
     
     
+# b = update_phone(telegram_id=1223, phone='phone')
+# print(b)
